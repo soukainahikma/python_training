695ec9388ec58c09a888a943caf6a1759bfa7edf
soukainahikma <soukainahikma@gmail.com>
2021-01-24 12:23:47 +0100

    Arithmetic Formatter

--- arithmetic_formatter.py
+++ arithmetic_formatter.py
@@ -1,10 +1,18 @@
 
-def problem_len(problems):
+def liste_len(problems):
 	len_ = len(problems)
 	if len_ > 5:
 		print("Error: Too many problems.")
-	return(0)
+		return(0)
+	return(1)
 def get_solution(x1,operation ,x2,ans = False):
+	try:
+		x1 =	int(x1)
+		x2 =	int(x2)
+	except:
+		return('Error: Numbers must only contain digits.')
+	if len(str(x1)) and len(str(x2)):
+		return('Error: Numbers cannot be more than four digits.')
 	if operation == "+":
 		if(ans == True):
 			return(x1 + x2)
@@ -17,13 +25,14 @@ def get_solution(x1,operation ,x2,ans = False):
 		 return("Error: Operator must be '+' or '-'.")
 
 def arithmetic_arranger(problems,get_ans = False):
-
-	error = problem_len(problems)
+	error = liste_len(problems)
+	
 	if error == 0:
 		return("")
-	arranged_problems = problems
-	return arranged_problems[1]
+	res = problems[0].split()
+
+	return get_solution(res[0],res[1] ,res[2],get_ans)
 	
 
-print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49","123 + 49","123 + 49"]))
-print(get_solution(1,"5",2))
+print(arithmetic_arranger(["32 + 6698"],True))
+#print(get_solution(1,"+",2))
